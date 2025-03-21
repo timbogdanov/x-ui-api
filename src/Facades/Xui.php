d85d94ad6ce21e49420b3057ad6fe985eeb2d336
tim-bogdanov <timb@snapsupplements.com>
2025-03-21 15:22:12 -0500

    update

--- src/Facades/Xui.php
+++ src/Facades/Xui.php
@@ -4,6 +4,19 @@ namespace TimBogdanov\Xui\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Xui
+ *
+ * @method static array getAllUsers(?string $inboundId = null)
+ * @method static array getUserByTgId(int $tgId, ?string $inboundId = null)
+ * @method static array getTrafficByUuid(string $uuid)
+ * @method static array updateUser(array $userData, ?string $inboundId = null)
+ * @method static array addUser(array $userData, ?string $inboundId = null)
+ * @method static array deleteUser(string $uuid, ?string $inboundId = null)
+ * @method static void reSync($user, ?string $inboundId = null)
+ *
+ * @see \TimBogdanov\Xui\XuiService
+ */
 class Xui extends Facade
 {
     protected static function getFacadeAccessor()
